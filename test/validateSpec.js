86d6744ad86db1073aa361f459a350d0dc0b63c3
blindgaenger <me@blindgaenger.net>
2016-10-03 09:46:50 +0200

    separated assert functions in validate

--- test/validateSpec.js
+++ test/validateSpec.js
@@ -3,297 +3,292 @@
 const expect = require('chai').expect
 
 const validateProp = require('../lib/validate').validateProp
-const validateValue = require('../lib/validate').validateValue
 const ProperiumError = require('../lib/error').ProperiumError
 
 describe('validate', () => {
-  describe('validateProp', () => {
-    describe('prop', () => {
-      it('rejects undefined props', () => {
-        const object = {}
-        const prop = { name: 'id' }
-        expect(() => validateProp('root', object, prop))
-          .to.throw(ProperiumError, 'undefined prop')
-          .and.to.have.property('prop', 'root.id')
-      })
+  it ('returns the object after validate', () => {
+    const object = { id: 'FOO' }
+    const prop = { name: 'id', name: 'id' }
+    const result = validateProp('root', object, prop)
+    expect(result).to.equal(object)
+  })
 
-      it('allows prop with a value of undefined', () => {
-        const object = { id: undefined }
-        const prop = { name: 'id' }
-        validateProp('root', object, prop)
-      })
+  describe('prop', () => {
+    it('rejects undefined props', () => {
+      const object = {}
+      const prop = { name: 'id', name: 'id' }
+      expect(() => validateProp('root', object, prop))
+        .to.throw(ProperiumError, 'undefined prop')
+        .and.to.have.property('prop', 'root.id')
     })
 
-    describe('defaultValue', () => {
-      it('allows undefined props', () => {
-        const object = {}
-        const prop = { name: 'id', defaultValue: 'DEFAULT' }
-        validateProp('root', object, prop)
-        expect(object.id).to.equal('DEFAULT')
-      })
-
-      it('allows the original value', () => {
-        const object = { id: 'ORIGINAL'}
-        const prop = { name: 'id', defaultValue: 'DEFAULT' }
-        validateProp('root', object, prop)
-        expect(object.id).to.equal('ORIGINAL')
-      })
-    })
-
-    it ('returns the object after validate', () => {
-      const object = { id: 'FOO' }
-      const prop = { name: 'id' }
-      const result = validateProp('root', object, prop)
-      expect(result).to.equal(object)
+    it('allows prop with a value of undefined', () => {
+      const object = { id: undefined }
+      const prop = { name: 'id', name: 'id' }
+      validateProp('root', object, prop)
     })
   })
 
-  describe('validateValue', () => {
-    describe('required', () => {
-      it('rejects an undefined value', () => {
-        const value = undefined
-        const prop = { required: true }
-        expect(() => validateValue(['root', 'id'], value, prop))
-          .to.throw(ProperiumError, 'required value')
-          .and.to.have.property('prop', 'root.id')
-      })
-
-      it('allows a defined value', () => {
-        const value = 'FOO'
-        const prop = { required: true }
-        validateValue(['root', 'id'], value, prop)
-      })
+  describe('defaultValue', () => {
+    it('allows undefined props', () => {
+      const object = {}
+      const prop = { name: 'id', name: 'id', defaultValue: 'DEFAULT' }
+      validateProp('root', object, prop)
+      expect(object.id).to.equal('DEFAULT')
     })
 
-    describe('oneOf', () => {
-      it('rejects an unknown value', () => {
-        const value = 'QUX'
-        const prop = { oneOf: ['FOO', 'BAR'] }
-        expect(() => validateValue(['root', 'id'], value, prop))
-          .to.throw(ProperiumError, 'unknown value')
-          .and.to.have.property('prop', 'root.id')
-      })
+    it('allows the original value', () => {
+      const object = { id: 'ORIGINAL'}
+      const prop = { name: 'id', name: 'id', defaultValue: 'DEFAULT' }
+      validateProp('root', object, prop)
+      expect(object.id).to.equal('ORIGINAL')
+    })
+  })
 
-      it('allows a known value', () => {
-        const value = 'FOO'
-        const prop = { oneOf: ['FOO', 'BAR'] }
-        validateValue(['root', 'id'], value, prop)
-      })
+  describe('required', () => {
+    it('rejects an undefined value', () => {
+      const object = { id: undefined }
+      const prop = { name: 'id', name: 'id', required: true }
+      expect(() => validateProp('root', object, prop))
+        .to.throw(ProperiumError, 'required value')
+        .and.to.have.property('prop', 'root.id')
     })
 
-    describe('type', () => {
-      describe('string', () => {
-        it('rejects', () => {
-          const value = 42
-          const prop = { type: 'string' }
-          expect(() => validateValue(['root', 'id'], value, prop))
-            .to.throw(ProperiumError, 'invalid type')
-            .and.to.have.property('prop', 'root.id')
-        })
+    it('allows a defined value', () => {
+      const object = { id: 'FOO' }
+      const prop = { name: 'id', name: 'id', required: true }
+      validateProp('root', object, prop)
+    })
+  })
 
-        it('allows', () => {
-          const value = 'FOO'
-          const prop = { type: 'string' }
-          validateValue(['root', 'id'], value, prop)
-        })
-      })
-
-      describe('number', () => {
-        it('rejects', () => {
-          const value = 'FOO'
-          const prop = { type: 'number' }
-          expect(() => validateValue(['root', 'id'], value, prop))
-            .to.throw(ProperiumError, 'invalid type')
-            .and.to.have.property('prop', 'root.id')
-        })
-
-        it('allows', () => {
-          const value = 42
-          const prop = { type: 'number' }
-          validateValue(['root', 'id'], value, prop)
-        })
-      })
-
-      describe('array', () => {
-        it('rejects', () => {
-          const value = 'FOO'
-          const prop = { type: 'array' }
-          expect(() => validateValue(['root', 'id'], value, prop))
-            .to.throw(ProperiumError, 'invalid type')
-            .and.to.have.property('prop', 'root.id')
-        })
-
-        it('allows', () => {
-          const value = []
-          const prop = { type: 'array' }
-          validateValue(['root', 'id'], value, prop)
-        })
-      })
-
-      describe('object', () => {
-        it('rejects', () => {
-          const value = 'FOO'
-          const prop = { type: 'object' }
-          expect(() => validateValue(['root', 'id'], value, prop))
-            .to.throw(ProperiumError, 'invalid type')
-            .and.to.have.property('prop', 'root.id')
-        })
-
-        it('allows', () => {
-          const value = {}
-          const prop = { type: 'object' }
-          validateValue(['root', 'id'], value, prop)
-        })
-      })
-
-      describe('Class', () => {
-        it('rejects', () => {
-          class ID {
-          }
-          const value = 'FOO'
-          const prop = { type: ID }
-          expect(() => validateValue(['root', 'id'], value, prop))
-            .to.throw(ProperiumError, 'invalid type')
-            .and.to.have.property('prop', 'root.id')
-        })
-
-        it('allows', () => {
-          class ID {
-          }
-          const value = new ID()
-          const prop = { type: ID }
-          validateValue(['root', 'id'], value, prop)
-        })
-      })
-
-      // TODO: describe.skip('array[]', () => {})
-      // TODO: describe.skip('enum[]', () => {})
+  describe('oneOf', () => {
+    it('rejects an unknown value', () => {
+      const object = { id: 'QUX' }
+      const prop = { name: 'id', oneOf: ['FOO', 'BAR'] }
+      expect(() => validateProp('root', object, prop))
+        .to.throw(ProperiumError, 'unknown value')
+        .and.to.have.property('prop', 'root.id')
     })
 
-    describe('length', () => {
-      it('rejects non length objects', () => {
-        const value = 42
-        const prop = { length: 1 }
-        expect(() => validateValue(['root', 'id'], value, prop))
-          .to.throw(ProperiumError, 'invalid length')
-          .and.to.have.property('prop', 'root.id')
-      })
-
-      it('rejects lower than length', () => {
-        const value = ['FOO']
-        const prop = { length: 2 }
-        expect(() => validateValue(['root', 'id'], value, prop))
-          .to.throw(ProperiumError, 'invalid length')
-          .and.to.have.property('prop', 'root.id')
-      })
-
-      it('rejects higher than length', () => {
-        const value = ['FOO', 'BAR']
-        const prop = { length: 1 }
-        expect(() => validateValue(['root', 'id'], value, prop))
-          .to.throw(ProperiumError, 'invalid length')
-          .and.to.have.property('prop', 'root.id')
-      })
-
-      it('allows exact length', () => {
-        const value = ['FOO']
-        const prop = { length: 1 }
-        validateValue(['root', 'id'], value, prop)
-      })
-
-      it('rejects lower than range', () => {
-        const value = ['FOO']
-        const prop = { length: [2, 3] }
-        expect(() => validateValue(['root', 'id'], value, prop))
-          .to.throw(ProperiumError, 'invalid length')
-          .and.to.have.property('prop', 'root.id')
-      })
-
-      it('rejects higher than range', () => {
-        const value = ['FOO', 'BAR']
-        const prop = { length: [0, 1] }
-        expect(() => validateValue(['root', 'id'], value, prop))
-          .to.throw(ProperiumError, 'invalid length')
-          .and.to.have.property('prop', 'root.id')
-      })
-
-      it('allows max length', () => {
-        const prop = { length: [undefined, 1] }
-        validateValue(['root', 'id'], [], prop)
-        validateValue(['root', 'id'], ['FOO'], prop)
-      })
-
-      it('allows min length', () => {
-        const prop = { length: [1, undefined] }
-        validateValue(['root', 'id'], ['FOO'], prop)
-        validateValue(['root', 'id'], ['FOO', 'BAR'], prop)
-      })
-
-      it('allows arrays', () => {
-        const prop = { length: [1, 1] }
-        validateValue(['root', 'id'], ['FOO'], prop)
-      })
-
-      it('allows strings', () => {
-        const prop = { length: [2, 3] }
-        validateValue(['root', 'id'], 'FO', prop)
-        validateValue(['root', 'id'], 'FOO', prop)
-      })
+    it('allows a known value', () => {
+      const object = { id: 'FOO' }
+      const prop = { name: 'id', oneOf: ['FOO', 'BAR'] }
+      validateProp('root', object, prop)
     })
+  })
 
-    describe('subtype', () => {
-      it('rejects non arrays', () => {
-        const value = 'FOO'
-        const prop = { subtype: 'string' }
-        expect(() => validateValue(['root', 'id'], value, prop))
-          .to.throw(ProperiumError, 'invalid subtype')
-          .and.to.have.property('prop', 'root.id')
-      })
-
-      it('rejects an invalid subtype', () => {
-        const value = [42]
-        const prop = { subtype: 'string' }
-        expect(() => validateValue(['root', 'id'], value, prop))
+  describe('type', () => {
+    describe('string', () => {
+      it('rejects', () => {
+        const object = { id: 42 }
+        const prop = { name: 'id', type: 'string' }
+        expect(() => validateProp('root', object, prop))
           .to.throw(ProperiumError, 'invalid type')
-          .and.to.have.property('prop', 'root.id.[0]')
+          .and.to.have.property('prop', 'root.id')
       })
 
-      it('allows a defined value', () => {
-        const value = ['FOO']
-        const prop = { subtype: 'string' }
-        validateValue(['root', 'id'], value, prop)
+      it('allows', () => {
+        const object = { id: 'FOO' }
+        const prop = { name: 'id', type: 'string' }
+        validateProp('root', object, prop)
       })
     })
 
-    describe('validate', () => {
-      it('rejects an invalid prop', () => {
-        class ID {
-          validate() {
-            throw new ProperiumError('SOURCE', 'MESSAGE')
-          }
-        }
-        const value = new ID()
-        const prop = { name: 'id' }
-        expect(() => validateValue(['root', 'id'], value, prop))
-          .to.throw(ProperiumError, 'MESSAGE')
-          .and.to.have.property('prop', 'SOURCE')
+    describe('number', () => {
+      it('rejects', () => {
+        const object = { id: 'FOO' }
+        const prop = { name: 'id', type: 'number' }
+        expect(() => validateProp('root', object, prop))
+          .to.throw(ProperiumError, 'invalid type')
+          .and.to.have.property('prop', 'root.id')
       })
 
-      it('allows a valid prop', () => {
-        class ID {
-          validate() {}
-        }
-        const value = new ID()
-        const prop = { name: 'id' }
-        validateValue(['root', 'id'], value, prop)
+      it('allows', () => {
+        const object = { id: 42 }
+        const prop = { name: 'id', type: 'number' }
+        validateProp('root', object, prop)
+      })
+    })
+
+    describe('array', () => {
+      it('rejects', () => {
+        const object = { id: 'FOO' }
+        const prop = { name: 'id', type: 'array' }
+        expect(() => validateProp('root', object, prop))
+          .to.throw(ProperiumError, 'invalid type')
+          .and.to.have.property('prop', 'root.id')
       })
 
-      it('ignores non-validatable', () => {
-        class ID {}
-        const value = new ID()
-        const prop = { name: 'id' }
-        validateValue(['root', 'id'], value, prop)
+      it('allows', () => {
+        const object = { id: [] }
+        const prop = { name: 'id', type: 'array' }
+        validateProp('root', object, prop)
       })
+    })
+
+    describe('object', () => {
+      it('rejects', () => {
+        const object = { id: 'FOO' }
+        const prop = { name: 'id', type: 'object' }
+        expect(() => validateProp('root', object, prop))
+          .to.throw(ProperiumError, 'invalid type')
+          .and.to.have.property('prop', 'root.id')
+      })
+
+      it('allows', () => {
+        const object = { id: {} }
+        const prop = { name: 'id', type: 'object' }
+        validateProp('root', object, prop)
+      })
+    })
+
+    describe('Class', () => {
+      it('rejects', () => {
+        class ID {
+        }
+        const object = { id: 'FOO' }
+        const prop = { name: 'id', type: ID }
+        expect(() => validateProp('root', object, prop))
+          .to.throw(ProperiumError, 'invalid type')
+          .and.to.have.property('prop', 'root.id')
+      })
+
+      it('allows', () => {
+        class ID {
+        }
+        const object = { id: new ID() }
+        const prop = { name: 'id', type: ID }
+        validateProp('root', object, prop)
+      })
+    })
+
+    // TODO: describe.skip('array[]', () => {})
+    // TODO: describe.skip('enum[]', () => {})
+  })
+
+  describe('length', () => {
+    it('rejects non length objects', () => {
+      const object = { id: 42 }
+      const prop = { name: 'id', length: 1 }
+      expect(() => validateProp('root', object, prop))
+        .to.throw(ProperiumError, 'invalid length')
+        .and.to.have.property('prop', 'root.id')
+    })
+
+    it('rejects lower than length', () => {
+      const object = { id: ['FOO'] }
+      const prop = { name: 'id', length: 2 }
+      expect(() => validateProp('root', object, prop))
+        .to.throw(ProperiumError, 'invalid length')
+        .and.to.have.property('prop', 'root.id')
+    })
+
+    it('rejects higher than length', () => {
+      const object = { id: ['FOO', 'BAR'] }
+      const prop = { name: 'id', length: 1 }
+      expect(() => validateProp('root', object, prop))
+        .to.throw(ProperiumError, 'invalid length')
+        .and.to.have.property('prop', 'root.id')
+    })
+
+    it('allows exact length', () => {
+      const object = { id: ['FOO'] }
+      const prop = { name: 'id', length: 1 }
+      validateProp('root', object, prop)
+    })
+
+    it('rejects lower than range', () => {
+      const object = { id: ['FOO'] }
+      const prop = { name: 'id', length: [2, 3] }
+      expect(() => validateProp('root', object, prop))
+        .to.throw(ProperiumError, 'invalid length')
+        .and.to.have.property('prop', 'root.id')
+    })
+
+    it('rejects higher than range', () => {
+      const object = { id: ['FOO', 'BAR'] }
+      const prop = { name: 'id', length: [0, 1] }
+      expect(() => validateProp('root', object, prop))
+        .to.throw(ProperiumError, 'invalid length')
+        .and.to.have.property('prop', 'root.id')
+    })
+
+    it('allows max length', () => {
+      const prop = { name: 'id', length: [undefined, 1] }
+      validateProp('root', { id: [] }, prop)
+      validateProp('root', { id: ['FOO'] }, prop)
+    })
+
+    it('allows min length', () => {
+      const prop = { name: 'id', length: [1, undefined] }
+      validateProp('root', { id: ['FOO'] }, prop)
+      validateProp('root', { id: ['FOO', 'BAR'] }, prop)
+    })
+
+    it('allows arrays', () => {
+      const prop = { name: 'id', length: [1, 1] }
+      validateProp('root', { id: ['FOO'] }, prop)
+    })
+
+    it('allows strings', () => {
+      const prop = { name: 'id', length: [2, 3] }
+      validateProp('root', { id: 'FO' }, prop)
+      validateProp('root', { id: 'FOO' }, prop)
+    })
+  })
+
+  describe('subtype', () => {
+    it('rejects non arrays', () => {
+      const object = { id: 'FOO' }
+      const prop = { name: 'id', subtype: 'string' }
+      expect(() => validateProp('root', object, prop))
+        .to.throw(ProperiumError, 'invalid subtype')
+        .and.to.have.property('prop', 'root.id')
+    })
+
+    it('rejects an invalid subtype', () => {
+      const object = { id: [42] }
+      const prop = { name: 'id', subtype: 'string' }
+      expect(() => validateProp('root', object, prop))
+        .to.throw(ProperiumError, 'invalid type')
+        .and.to.have.property('prop', 'root.id.[0]')
+    })
+
+    it('allows a defined value', () => {
+      const object = { id: ['FOO'] }
+      const prop = { name: 'id', subtype: 'string' }
+      validateProp('root', object, prop)
+    })
+  })
+
+  describe('validate', () => {
+    it('rejects an invalid prop', () => {
+      class ID {
+        validate() {
+          throw new ProperiumError('SOURCE', 'MESSAGE')
+        }
+      }
+      const object = { id: new ID() }
+      const prop = { name: 'id', name: 'id' }
+      expect(() => validateProp('root', object, prop))
+        .to.throw(ProperiumError, 'MESSAGE')
+        .and.to.have.property('prop', 'SOURCE')
+    })
+
+    it('allows a valid prop', () => {
+      class ID {
+        validate() {}
+      }
+      const object = { id: new ID() }
+      const prop = { name: 'id', name: 'id' }
+      validateProp('root', object, prop)
+    })
+
+    it('ignores non-validatable', () => {
+      class ID {}
+      const object = { id: new ID() }
+      const prop = { name: 'id', name: 'id' }
+      validateProp('root', object, prop)
     })
   })
 })
